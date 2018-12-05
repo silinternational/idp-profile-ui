5d87a0486364935d16e19104d445fe3b43e7bba0
Billy Clark <billy_clark@sil.org>
2018-12-05 13:50:09 -0500

    some users need additional anonymity

---
+++ src/profile/DoNotDiscloseCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="layout column">
+    <v-card-title primary-title>
+      <h3 class="headline">{{ $vuetify.t('$vuetify.profile.index.dndCard.title') }}</h3>
+    </v-card-title>
+
+    <v-card-text>{{ $vuetify.t('$vuetify.profile.index.dndCard.info') }}</v-card-text>
+
+    <v-card-actions>
+      <v-spacer/>
+
+      <v-btn @click="toggle()" color="primary" flat>
+        <span v-if="dnd == 'yes'">{{ $vuetify.t('$vuetify.profile.index.dndCard.button.disable') }}</span>
+        <span v-else>{{ $vuetify.t('$vuetify.profile.index.dndCard.button.enable') }}</span>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template >
+
+<script>
+export default {
+  props: ["dnd"],
+  methods: {
+    toggle: function() {
+      this.$API.put("user/me", {
+        do_not_disclose: this.dnd == "yes" ? "no" : "yes"
+      });
+    }
+  }
+};
+</script>
--- src/profile/Index.vue
+++ src/profile/Index.vue
@@ -3,40 +3,41 @@
     <template slot="header">
       <div class="layout column">
         {{ $vuetify.t('$vuetify.profile.index.header', $config.idpName) }}
-        
-        <Attribute v-if="$user.password_meta.last_login" 
-                   :name="$vuetify.t('$vuetify.profile.index.lastLogin')" 
-                   :value="$user.password_meta.last_login | format" />      
+        <Attribute
+          v-if="$user.password_meta.last_login"
+          :name="$vuetify.t('$vuetify.profile.index.lastLogin')"
+          :value="$user.password_meta.last_login | format"
+        />
       </div>
-      
-      <v-spacer />
+
+      <v-spacer/>
       <ProfileProgress :profile="{recoveryMethods, mfas, user: $user}"/>
     </template>
 
     <v-layout row wrap>
-      <PasswordCard :meta="$user.password_meta" class="mx-3 mb-4" />
-      <PasswordRecoveryCard :methods="recoveryMethods" class="mx-3 mb-4" />
+      <PasswordCard :meta="$user.password_meta" class="mx-3 mb-4"/>
+      <PasswordRecoveryCard :methods="recoveryMethods" class="mx-3 mb-4"/>
+      <DoNotDiscloseCard :dnd="$user.do_not_disclose" class="mx-3 mb-4"/>
     </v-layout>
 
-    <v-subheader class="py-5">
-      {{ $vuetify.t('$vuetify.profile.index.2sv') }}
-    </v-subheader>
+    <v-subheader class="py-5">{{ $vuetify.t('$vuetify.profile.index.2sv') }}</v-subheader>
     <v-layout row wrap>
-      <TotpCard :meta="totp" class="mx-3 mb-4" />
-      <U2fCard :meta="u2f" class="mx-3 mb-4" />
-      <BackupCodeCard :meta="codes" class="mx-3 mb-4" />
+      <TotpCard :meta="totp" class="mx-3 mb-4"/>
+      <U2fCard :meta="u2f" class="mx-3 mb-4"/>
+      <BackupCodeCard :meta="codes" class="mx-3 mb-4"/>
     </v-layout>
   </BasePage>
 </template>
 
 <script>
-import Attribute from './Attribute';
-import ProfileProgress from './ProfileProgress';
-import PasswordCard from './PasswordCard';
-import PasswordRecoveryCard from './PasswordRecoveryCard';
-import TotpCard from './TotpCard';
-import U2fCard from './U2fCard';
-import BackupCodeCard from './BackupCodeCard';
+import Attribute from "./Attribute";
+import ProfileProgress from "./ProfileProgress";
+import PasswordCard from "./PasswordCard";
+import PasswordRecoveryCard from "./PasswordRecoveryCard";
+import TotpCard from "./TotpCard";
+import U2fCard from "./U2fCard";
+import BackupCodeCard from "./BackupCodeCard";
+import DoNotDiscloseCard from "./DoNotDiscloseCard";
 
 export default {
   components: {
@@ -46,23 +47,24 @@ export default {
     PasswordRecoveryCard,
     TotpCard,
     U2fCard,
-    BackupCodeCard
+    BackupCodeCard,
+    DoNotDiscloseCard
   },
   data: () => ({
     recoveryMethods: [],
     mfas: []
   }),
   async created() {
-    const methodPromise = this.$API.get('method');
-    const mfaPromise = this.$API.get('mfa');
+    const methodPromise = this.$API.get("method");
+    const mfaPromise = this.$API.get("mfa");
 
     this.recoveryMethods = await methodPromise;
     this.mfas = await mfaPromise;
   },
   computed: {
-    totp: vm => vm.mfas.find(mfa => mfa.type == 'totp') || {},
-    u2f: vm => vm.mfas.find(mfa => mfa.type == 'u2f') || {},
-    codes: vm => vm.mfas.find(mfa => mfa.type == 'backupcode') || {}
+    totp: vm => vm.mfas.find(mfa => mfa.type == "totp") || {},
+    u2f: vm => vm.mfas.find(mfa => mfa.type == "u2f") || {},
+    codes: vm => vm.mfas.find(mfa => mfa.type == "backupcode") || {}
   }
 };
 </script>
