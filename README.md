a09e5f2b51e59cf5f68790114d8b7e25a6f52251
Billy Clark <billy_clark@sil.org>
2018-12-20 14:00:09 -0500

    new users without passwords and a valid invitation code should still be authenticated.

--- README.md
+++ README.md
@@ -26,9 +26,15 @@ i18n support is built-in however the translation files must be generated and dro
 
 ### Authentication
 
-1.  An IdP infrastructure will be running locally and is required for authentication into the app. If needed, it can be accessed at the traefik-configured domain, e.g., [https://profile-api.gtis.guru/auth/login?client_id=abc123](https://profile-api.gtis.guru/auth/login?client_id=abc123).
+An IdP infrastructure will be running locally and is required for authentication into the app's protected pages.
+
+#### Existing user
 1.  Username: **a**
 1.  Password: **a**
+
+#### New user (has no password yet)
+1. Enter app with a special "invitation" link, e.g., [https://profile.gtis.guru/profile/intro?invite=2b2d424e-8cb0-49c7-8c0b-7f6603INVITE](https://profile.gtis.guru/profile/intro?invite=2b2d424e-8cb0-49c7-8c0b-7f6603INVITE)
+1. Username: **b**
 
 ## Local development
 
--- development/m991231_235959_insert_test_users.php
+++ development/m991231_235959_insert_test_users.php
@@ -9,13 +9,19 @@ class m991231_235959_insert_test_users extends Migration
     public function safeUp()
     {
         $this->batchInsert('{{user}}',
-            ['id','uuid'                                ,'employee_id','first_name','last_name','username','email'              ,'active','locked','last_changed_utc'   ,'last_synced_utc'    ,'require_mfa','nag_for_mfa_after'    ,'nag_for_method_after' ],[
-            [ 1  ,'2b2d424e-8cb0-49c7-8c0b-7f660340f5fa','11111'      ,'Testy'     ,'Testerson','a'       ,'primary@example.org','yes'   ,'no'    , MySqlDateTime::now(), MySqlDateTime::now(),'no'         , MySqlDateTime::today(), MySqlDateTime::today()],
+            ['id','uuid'                                ,'employee_id','first_name','last_name' ,'username','email'               ,'active','locked','last_changed_utc'   ,'last_synced_utc'    ,'require_mfa','nag_for_mfa_after'    ,'nag_for_method_after' ],[
+            [ 1  ,'2b2d424e-8cb0-49c7-8c0b-7f660340f5fa','11111'      ,'Testy'     ,'Testerson' ,'a'       ,'primaryA@example.org','yes'   ,'no'    , MySqlDateTime::now(), MySqlDateTime::now(),'no'         , MySqlDateTime::today(), MySqlDateTime::today()],
+            [ 2  ,'2b2d424e-8cb0-49c7-8c0b-7f660340f5fb','22222'      ,'Firsty'    ,'Firsterson','b'       ,'primaryB@example.org','yes'   ,'no'    , MySqlDateTime::now(), MySqlDateTime::now(),'no'         , MySqlDateTime::today(), MySqlDateTime::today()],
         ]);
 
         $this->batchInsert('{{password}}',
             ['id','user_id','hash'                                                        ,'created_utc'        ,'expires_on'                       ,'grace_period_ends_on'             ],[
             [ 1  , 1       ,'$2y$10$rKbAp0M8gewGpQKhD.U6qOSGDlMqKFkxK9tQZ15SZoieqYHYNsD/y', MySqlDateTime::now(), MySqlDateTime::relative('+1 year'), MySqlDateTime::relative('+1 year')], // password is 'a'
+        ]);
+
+        $this->batchInsert('{{invite}}',
+            ['id','user_id','uuid'                                ,'created_utc'        ,'expires_on'                       ],[
+            [ 1  , 2       ,'2b2d424e-8cb0-49c7-8c0b-7f6603INVITE', MySqlDateTime::now(), MySqlDateTime::relative('+1 year')],
         ]);
 
         $this->update('{{user}}', ['current_password_id' => 1], 'id=1');
@@ -27,8 +33,12 @@ class m991231_235959_insert_test_users extends Migration
             'user_id' => [1]
         ]);
 
+        $this->delete('{{invite}}', [
+            'user_id' => [2]
+        ]);
+
         $this->delete('{{user}}', [
-            'id' => [1]
+            'id' => [1, 2]
         ]);
     }
 }
