3066618df303b65b1f7fd43aa68e700e272375c2
Billy Clark <billy_clark@sil.org>
2019-02-13 11:20:36 -0500

    'Invalid Date' showing to user when data was missing.

--- src/global/filters.js
+++ src/global/filters.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import { format } from 'date-fns'
 
 Vue.filter('format', (date, fmt = 'dddd, MMMM Do YYYY') =>
-  format(date, fmt)
+  date ? format(date, fmt) : '–'
 )
 Vue.filter('formatLong', (date, fmt = 'dddd, MMMM Do YYYY, h:mm:ss a') =>
-  format(date, fmt)
+  date ? format(date, fmt) : '–'
 )
