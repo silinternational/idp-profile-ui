8a8f1a5667fd405e640ff000e0e7fcd8e04936be
Billy Clark <billy_clark@sil.org>
2019-02-14 10:25:09 -0500

    users wanted a clearer explanation of the pencil icon

--- src/profile/MfaCardLabel.vue
+++ src/profile/MfaCardLabel.vue
@@ -1,13 +1,16 @@
 <template>
   <v-layout v-if="editing" row align-center>
-    <v-text-field v-if="editing" v-model="newLabel" autofocus @focus="$event.target.select()"/>
+    <v-text-field v-if="editing" v-model="newLabel" @keyup.enter="save" autofocus @focus="$event.target.select()"/>
 
     <v-icon @click="save" color="success" small class="pl-2">check</v-icon>
     <v-icon @click="cancel" color="error" small class="pl-1">close</v-icon>
   </v-layout>
   <h3 v-else class="headline pl-3 layout row align-center">
     {{ label }}
-    <v-icon v-if="id" @click="edit" color="info" small class="pl-2">edit</v-icon>
+    <v-tooltip right>
+      <v-icon v-if="id" slot="activator" @click="edit" color="info" small class="layout row align-center pl-3">edit</v-icon>
+      {{ $vuetify.t('$vuetify.profile.index.rename') }}
+    </v-tooltip>
   </h3>
 </template>
 
