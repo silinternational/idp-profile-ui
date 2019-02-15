2117465858082865ff908c525610916d229460a0
Billy Clark <billy_clark@sil.org>
2019-02-15 13:22:57 -0500

    content was missing in final wizard step

--- src/profile/Complete.vue
+++ src/profile/Complete.vue
@@ -2,11 +2,20 @@
   <ProfileWizard ref="wizard">
     <BasePage>
       <template slot="header">
-        {{ $vuetify.t('$vuetify.profile.complete.header', $root.idpConfig.idpName) }}
+        {{ $vuetify.t('$vuetify.profile.complete.header') }}
       </template>
 
-      <p>
-        {{ $vuetify.t('$vuetify.profile.complete.congrats', $root.idpConfig.idpName, $user.first_name) }}
+      <p class="layout row">
+        <v-icon color="success" class="mr-3">done</v-icon> 
+        {{ $vuetify.t('$vuetify.profile.complete.info', $user.first_name, $root.idpConfig.idpName) }}
+      </p>
+
+      <p v-if="unverifiedEmails.length" class="layout row">
+        <v-icon color="warning" class="mr-3">email</v-icon> 
+        <span>
+          {{ $vuetify.t('$vuetify.profile.complete.unverifiedEmails') }}
+          <span class="font-weight-bold">({{ unverifiedEmails | joined }})</span>
+        </span>
       </p>
     </BasePage>
 
@@ -14,7 +23,7 @@
       <v-spacer></v-spacer>
       
       <v-btn to="/profile" color="primary" flat outline>
-        {{ $vuetify.t('$vuetify.profile.complete.button.newProfile') }}
+        {{ $vuetify.t('$vuetify.profile.complete.button.profile') }}
       </v-btn>
     </ButtonBar>
   </ProfileWizard>
@@ -26,6 +35,12 @@ import ProfileWizard from '@/profile/ProfileWizard'
 export default {
   components: {
     ProfileWizard
+  },
+  filters: {
+    joined: emails => emails.map(email => email.value).join(', ')
+  },
+  computed: {
+    unverifiedEmails: vm => vm.$user.recoveryMethods.personal.filter(m => ! m.verified),
   },
   mounted() {
     this.$refs.wizard.complete()
