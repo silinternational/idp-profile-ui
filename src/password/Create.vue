57db62f7e0337b9ee029dc2fd4b77ec7e46a89b0
Billy Clark <billy_clark@sil.org>
2019-03-22 15:29:08 -0400

    new users wondered what their uname was here a lot.

--- src/password/Create.vue
+++ src/password/Create.vue
@@ -9,6 +9,8 @@
               frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-3" /> -->
 
       <v-form @submit.prevent="save" ref="form">
+        <p><span class="body-2">{{ $vuetify.t('$vuetify.password.create.username', $root.idpConfig.idpName) }}</span> {{ $user.idp_username }}</p>
+
         <BaseTextField type="password" :label="$vuetify.t('$vuetify.password.create.pwInput')" v-model="password" 
                        :rules="rules" validate-on-blur @keyup.enter="blur" autofocus />
 
