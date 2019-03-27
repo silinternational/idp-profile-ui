85026a29733b776ea61b7b0bc3e2b4a11039982b
Billy Clark <billy_clark@sil.org>
2019-03-27 13:47:19 -0400

    password expiration was not changing in UI when a user removed all mfas.

--- src/profile/MfaCardRemove.vue
+++ src/profile/MfaCardRemove.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn @click="remove(id)" :loading="removing" color="error" flat outline>
+  <v-btn @click="remove(id)" color="error" flat outline>
     {{ $vuetify.t('$vuetify.global.button.remove') }}
   </v-btn>
 </template>
@@ -9,19 +9,14 @@ import { remove } from '@/global/mfa';
 
 export default {
   props: ['id'],
-  data: () => ({
-    removing: false
-  }),
   methods: {
     async remove(id) {
       if (confirm(this.$vuetify.t('$vuetify.global.areYouSure'))) {
-        this.removing = true
-
         await remove(id)
 
-        this.removing = false
+        this.$router.go() // needs to result in a page reload since the password expiration is affected by the absence or presence of an mfa.
       }
-    }
-  }
+    },
+  },
 }
 </script>
