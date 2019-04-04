e8cc9c5e13bf1f9d3f2c8e2604c13ae224b632b1
Billy Clark <billy_clark@sil.org>
2019-04-04 15:05:45 -0400

    malformed page was generating alerts

--- src/global/PageNotFound.vue
+++ src/global/PageNotFound.vue
@@ -1,5 +1,9 @@
 <template>
   <BasePage>
+    <template slot="header">
+      {{ $vuetify.t('$vuetify.pageNotFound.header') }}
+    </template>
+
     <img src="@/assets/riddler-geeky-world.jpg">
   </BasePage>
 </template>
