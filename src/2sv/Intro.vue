b9457ca1f7e8eb23bcecf83f59589379755cbc13
Billy Clark <billy_clark@sil.org>
2019-04-12 12:18:45 -0400

    didn't like the way the content broke compared to content above it

--- src/2sv/Intro.vue
+++ src/2sv/Intro.vue
@@ -12,12 +12,10 @@
       <iframe width="560" height="315" src="https://www.youtube.com/embed/N1CNo4AZLRQ?rel=0" 
               frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-3" />
 
-      <v-layout row align-center>
+      <p class="ma-0 body-2 layout row">
         <v-icon color="info" class="mr-2">info</v-icon>
-        <p class="ma-0 body-2">
-          {{ $vuetify.t('$vuetify.2sv.intro.incentive') }}
-        </p>
-      </v-layout>
+        {{ $vuetify.t('$vuetify.2sv.intro.incentive') }}
+      </p>
     </BasePage>
 
     <ButtonBar>
