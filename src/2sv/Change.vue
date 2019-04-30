77eae76c0e28a97fdae0a820c96c389b10afc177
Billy Clark <billy_clark@sil.org>
2019-04-30 15:37:25 -0400

    users found 2sv removal for lost/new use cases to be abrupt

--- src/2sv/Change.vue
+++ src/2sv/Change.vue
@@ -4,34 +4,48 @@
       {{ $vuetify.t('$vuetify.2sv.change.header') }}
     </template>
 
-    <p v-if="removing">
-      {{ $vuetify.t('$vuetify.2sv.change.removing') }}
-    </p>
-    <p v-else>
-      {{ $vuetify.t('$vuetify.2sv.change.removed') }}
+    <v-alert :value="isLastOne" type="error">
+      <span>{{ $vuetify.t('$vuetify.2sv.change.lastOne') }}</span>
+    </v-alert>
+
+    <p class="py-3">
+      {{ $vuetify.t('$vuetify.2sv.change.info', label) }}
     </p>
 
     <ButtonBar>
+      <v-btn to="/profile" flat outline> 
+        {{ $vuetify.t('$vuetify.global.button.no') }}
+      </v-btn>
+
       <v-spacer></v-spacer>
       
-      <v-btn to="/profile" color="primary" flat outline> 
-        {{ $vuetify.t('$vuetify.2sv.change.button.ok') }}
+      <v-btn @click="yes(requested.id)" color="primary" flat outline> 
+        {{ $vuetify.t('$vuetify.global.button.yes') }}
       </v-btn>
     </ButtonBar>
   </BasePage>
 </template>
 
 <script>
-import { remove } from '@/global/mfa';
+import { find, remove, mfa } from '@/global/mfa';
 
 export default {
   data: () => ({
-    removing: true
+    requested: {},
+  isLastOne: mfa.numVerified === 1
   }),
-  async created() {
-    await remove(this.$route.params.id)
+  computed: {
+    label: vm => vm.requested && vm.requested.label || '2SV'
+  },
+  methods: {
+    async yes(id) {
+      await remove(id)
 
-    this.removing = false
-  }
+      this.$router.push('/2sv/removed')
+    },
+  },
+  created() {
+    this.requested = find(this.$route.params.id)
+  },
 }
 </script>
---
+++ src/2sv/Removed.vue
@@ -0,0 +1,23 @@
+<template>
+  <BasePage>
+    <template v-slot:header>
+      {{ $vuetify.t('$vuetify.2sv.removed.header') }}
+    </template>
+
+    <p class="py-3">
+      {{ $vuetify.t('$vuetify.2sv.removed.info') }}
+    </p>
+
+    <ButtonBar>
+      <v-btn to="/profile" color="warning" flat outline> 
+        {{ $vuetify.t('$vuetify.2sv.removed.button.later') }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+      
+      <v-btn to="/2sv/intro" color="primary" flat outline> 
+        {{ $vuetify.t('$vuetify.global.button.yes') }}
+      </v-btn>
+    </ButtonBar>
+  </BasePage>
+</template>
