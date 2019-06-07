b1fd7cea9d970f6feb85b6e0ff2d025f19217600
Billy Clark <billy_clark@sil.org>
2019-06-07 11:34:43 -0400

    french/korean translations finished

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,10 +5,17 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   lang: {
-    locales: locales
+    locales: locales,
+    current: determineCurrentLanguage()
   },
   theme: {
     primary: `${process.env.VUE_APP_PRIMARY_COLOR}`,
     secondary: `${process.env.VUE_APP_SECONDARY_COLOR}`,
   },
 })
+
+function determineCurrentLanguage() {
+  const mainLangOnly = navigator.language.substring(0,2)
+
+  return locales[mainLangOnly] ? mainLangOnly : 'en'
+}
