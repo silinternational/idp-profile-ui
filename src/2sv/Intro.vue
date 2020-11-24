4dd64f7a2cd58537b80b006158541520f64456cc
hobbitronics <miguel.dw@gmail.com>
2020-11-24 16:27:18 -0500

    migrated to v-row, v-col removed layout row class

--- src/2sv/Intro.vue
+++ src/2sv/Intro.vue
@@ -5,17 +5,24 @@
         {{ $vuetify.lang.t('$vuetify.2sv.intro.header') }}
       </template>
 
-      <p>
-        {{ $vuetify.lang.t('$vuetify.2sv.intro.info') }}
-      </p>
+      <v-row justify="center">
+        <v-col cols="auto">
+          {{ $vuetify.lang.t('$vuetify.2sv.intro.info') }}
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/N1CNo4AZLRQ?rel=0" 
+                frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-3" />
+      </v-row>
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/N1CNo4AZLRQ?rel=0" 
-              frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-3" />
-
-      <p class="ma-0 body-2 layout row">
-        <v-icon color="info" class="mr-2">mdi-information</v-icon>
-        {{ $vuetify.lang.t('$vuetify.2sv.intro.incentive') }}
-      </p>
+      <v-row justify="center" align="center" class="mx-16">
+        <v-col cols="1" class="text-right">
+          <v-icon color="info">mdi-information</v-icon>
+        </v-col>
+        <v-col cols="8">
+          {{ $vuetify.lang.t('$vuetify.2sv.intro.incentive') }}
+        </v-col>
+      </v-row>
     </BasePage>
 
     <ButtonBar>
