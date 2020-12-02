9a8039aa7d367d05b395aacfe8c541562c4a70d7
hobbitronics <miguel.dw@gmail.com>
2020-12-02 10:58:19 -0500

    intro page formatting was broken: added v-row/col

--- src/profile/Intro.vue
+++ src/profile/Intro.vue
@@ -4,23 +4,33 @@
       {{ $vuetify.lang.t('$vuetify.profile.intro.header', $root.idpConfig.idpName, $user.first_name) }}
     </template>
 
-    <p>
-      {{ $vuetify.lang.t('$vuetify.profile.intro.explanation') }}
-    </p>
+    <v-row justify="center">
+      <p class="mx-4">
+        {{ $vuetify.lang.t('$vuetify.profile.intro.explanation') }}
+      </p>
 
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/kXDeNWDlEqk?rel=0" 
-            frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-4" />
-
-    <p>
-      {{ $vuetify.lang.t('$vuetify.profile.intro.username') }} <strong class="body-2">{{ $user.idp_username }}</strong>
-    </p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/kXDeNWDlEqk?rel=0" 
+              frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen class="pa-4" />
+    </v-row>
 
     <ButtonBar>
       <v-spacer></v-spacer>
-      
-      <v-btn to="/password/create" color="primary" outlined>
-        {{ $vuetify.lang.t('$vuetify.profile.intro.button.start') }}
-      </v-btn>
+
+      <v-col cols="auto" class="mr-4">
+        <p>
+          {{ $vuetify.lang.t('$vuetify.profile.intro.username') }} <strong class="body-2">{{ $user.idp_username }}</strong>
+        </p>
+      </v-col>
+
+      <v-spacer></v-spacer>
+
+      <v-col cols="auto">
+        <v-btn to="/password/create" color="primary" outlined>
+          {{ $vuetify.lang.t('$vuetify.profile.intro.button.start') }}
+        </v-btn>
+      </v-col>
+
+      <v-spacer></v-spacer>
     </ButtonBar>
   </BasePage>
 </template>
