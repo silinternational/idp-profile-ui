54cbc48de425e4248c96aa9b0124f7230c8b43a6
hobbitronics <miguel.dw@gmail.com>
2020-12-09 09:55:42 -0500

    feedback

--- src/profile/ProfileProgress.vue
+++ src/profile/ProfileProgress.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-row no-gutters justify="end">
-    <v-col cols="auto">
+    <div class="display-1 d-flex flex-column align-end">
       <p class="body-2 text-right">{{ $vuetify.lang.t('$vuetify.profile.progress.header') }}</p>
 
-    <meter low="0.5" high="0.8" optimum="1" :value="complete" class="pb-4"/>
+      <meter low="0.5" high="0.8" optimum="1" :value="complete" class="pb-4"/>
 
-    <a v-if="complete < 1 && ! profile.alternates.length" href="#/password/recovery" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addMethod') }}</a>
-    <a v-if="complete < 1 && ! profile.mfa.totp.id" href="#/2sv/smartphone/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addTotp') }}</a>
-    <a v-if="complete < 1 && ! profile.mfa.u2f.id" href="#/2sv/usb-security-key/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addU2f') }}</a>
-    <a v-if="complete < 1 && ! profile.mfa.backup.id" href="#/2sv/printable-backup-codes/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addCodes') }}</a>
-    </v-col>
-  </v-row>
+      <a v-if="complete < 1 && ! profile.alternates.length" href="#/password/recovery" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addMethod') }}</a>
+      <a v-if="complete < 1 && ! profile.mfa.totp.id" href="#/2sv/smartphone/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addTotp') }}</a>
+      <a v-if="complete < 1 && ! profile.mfa.u2f.id" href="#/2sv/usb-security-key/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addU2f') }}</a>
+      <a v-if="complete < 1 && ! profile.mfa.backup.id" href="#/2sv/printable-backup-codes/intro" class="caption">{{ $vuetify.lang.t('$vuetify.profile.progress.addCodes') }}</a>
+    </div>
 </template>
 
 <script>
