4608424b2df93079466fd9933697ead938709cef
Billy Clark <billy_clark@sil.org>
2020-12-17 08:46:02 -0500

    note unsupported browsers

--- src/2sv/key/Insert.vue
+++ src/2sv/key/Insert.vue
@@ -1,13 +1,17 @@
 <template>
   <ProfileWizard>
     <BasePage>
-      <template v-slot:header>
+      <template v-if="isSupported" v-slot:header>
         {{ $vuetify.lang.t('$vuetify.2sv.key.insert.header') }}
       </template>
+      <template v-else v-slot:header>
+        {{ $vuetify.lang.t('$vuetify.2sv.key.insert.nosupport.header') }}
+      </template>
 
-      <figure class="pa-4">
+      <figure v-if="isSupported" class="pa-4">
         <v-img contain src="@/assets/insert-usb-security-key.png" alt="A usb key inserted into a usb port."/>
       </figure>
+      <p v-else>{{ $vuetify.lang.t('$vuetify.2sv.key.insert.nosupport.info') }}</p>
     </BasePage>
 
     <ButtonBar>
@@ -17,8 +21,11 @@
 
       <v-spacer></v-spacer>
       
-      <v-btn to="/2sv/usb-security-key/touch" color="primary" outlined> 
+      <v-btn v-if="isSupported" to="/2sv/usb-security-key/touch" color="primary" outlined>
         {{ $vuetify.lang.t('$vuetify.2sv.key.insert.button.ok') }}
+      </v-btn>
+      <v-btn v-else to="/2sv/printable-backup-codes/intro" color="primary" outlined> 	
+        {{ $vuetify.lang.t('$vuetify.global.button.skip') }}	
       </v-btn>
     </ButtonBar>
   </ProfileWizard>
@@ -31,5 +38,8 @@ export default {
   components: {
     ProfileWizard,
   },
+  data: () => ({	
+    isSupported: (window.chrome || ! window.navigator.userAgent.match(/safari/i))
+  }),
 }
 </script>
