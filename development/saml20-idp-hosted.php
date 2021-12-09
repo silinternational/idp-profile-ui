53a8c26076576589edf00763621d8f17eb5dcf8c
Matt Henderson <matt_henderson@sil.org>
2021-12-09 17:35:48 -0500

    Enable the MFA module auth. proc.

--- development/saml20-idp-hosted.php
+++ development/saml20-idp-hosted.php
@@ -5,4 +5,19 @@ use Sil\Psr3Adapters\Psr3SamlLogger;
 $metadata['profile-idp'] = [
     'host' => '__DEFAULT__',
     'auth' => 'silauth',
+    'authproc' => [
+        10 => [
+            // Required:
+            'class' => 'mfa:Mfa',
+            'employeeIdAttr' => 'employeeNumber',
+            'idBrokerAccessToken' => Env::get('ID_BROKER_ACCESS_TOKEN'),
+            'idBrokerAssertValidIp' => Env::get('ID_BROKER_ASSERT_VALID_IP'),
+            'idBrokerBaseUri' => Env::get('ID_BROKER_BASE_URI'),
+            'idBrokerTrustedIpRanges' => Env::get('ID_BROKER_TRUSTED_IP_RANGES'),
+            'mfaSetupUrl' => Env::get('MFA_SETUP_URL'),
+
+            // Optional:
+            'loggerClass' => Psr3SamlLogger::class,
+        ],
+    ],
 ];
