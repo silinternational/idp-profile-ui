adae1f47e3bd8c5b495ad1b0216a1f8ddc386686
Michael Wilson <michael_wilson@sil.org>
2022-07-20 21:15:04 +0930

    replace settings cards w/ settings list

--- src/profile/Account.vue
+++ src/profile/Account.vue
@@ -1,38 +1,42 @@
 <template>
   <BasePage>
     <template v-slot:header>
-      {{ $vuetify.lang.t('$vuetify.profile.index.header', $root.idpConfig.idpName) }}
+      <!-- Todo add this to locales -->
+      Account Settings
     </template>
 
-    <v-row>
-      <v-col cols="6">
-        <Attribute :name="$vuetify.lang.t('$vuetify.profile.index.username')" :value="$user.idp_username" sameline />
-        <Attribute :name="$vuetify.lang.t('$vuetify.profile.index.lastLogin')" :value="$user.last_login | format" sameline />
-        <Attribute :name="$vuetify.lang.t('$vuetify.profile.index.manager')" :value="$user.manager_email" sameline />
-      </v-col>
+    <!-- A list of account settings -->
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.url"
+        :exact="true"
+        :active-class="'primary--text'"
+        class="text-capitalize"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        
+        <v-list-item two-line>
+          <v-list-item-content> 
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ item.secondary }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-col cols="6">
-        <ProfileProgress :profile="{user: $user, alternates, mfa}"/>
-      </v-col>
-    </v-row>
+        <v-list-item-icon v-if="item.hasPencil">
+          <v-icon>mdi-pencil</v-icon>
+        </v-list-item-icon>
+
+        <v-switch v-if="item.hasSwitch" v-model="hidden" @click="toggle()" />
+      </v-list-item>
+    </v-list>
 
     <v-alert :value="hasUnverifiedEmails" type="error">
       <span>{{ $vuetify.lang.t('$vuetify.profile.index.unverifiedEmails') }}</span>
     </v-alert>
-
-    <v-row>
-      <v-col cols="12" sm="6" md="4">
-        <PasswordCard :meta="$user.password_meta"/>
-      </v-col>
-
-      <v-col cols="12" sm="6" md="4">
-        <PasswordRecoveryCard :methods="alternates"/>
-      </v-col>
-
-      <v-col cols="12" sm="6" md="4">
-        <DoNotDiscloseCard :dnd="$user.hide"/>
-      </v-col>
-    </v-row>
   </BasePage>
 </template>
 
@@ -54,12 +58,38 @@ export default {
     DoNotDiscloseCard,
     Attribute,
   },
-  data: () => ({
-    alternates: recoveryMethods.alternates,
-    mfa,
-  }),
+  data () {
+    return {
+      alternates: recoveryMethods.alternates,
+      mfa,
+      toggling: false,
+      hidden: this.$user.hide === 'yes'
+    }
+  },
   computed: {
     hasUnverifiedEmails: vm => vm.alternates.some(m => ! m.verified),
+    items () {
+    return [
+      //Todo update/add titles to locales
+      { title: this.$vuetify.lang.t('$vuetify.profile.index.username'), icon: 'mdi-account-circle', secondary: this.$user.idp_username },
+      { title: 'preferred language', icon: 'mdi-translate' , secondary: navigator.language, hasPencil: true },
+      { title: this.$vuetify.lang.t('$vuetify.profile.index.passwordCard.title'), icon: 'mdi-account-key', url: '/password/create', hasPencil: true, secondary: new Date(this.$user?.password_meta?.expires).toLocaleDateString()},
+      { title:  this.$vuetify.lang.t('$vuetify.profile.index.passwordRecoveryCard.title'), icon: 'mdi-email-outline', url: '/password/recovery', hasPencil: true }, //Todo add tooltip
+      { title: this.$vuetify.lang.t('$vuetify.profile.index.manager'), icon: 'mdi-account-multiple', secondary: this.$user.manager_email }, //Todo add tooltip
+      { title:  this.$vuetify.lang.t('$vuetify.profile.index.dndCard.title') , icon: 'mdi-security', hasSwitch: true }, // Todo add tooltip
+    ]
+    },
   },
+  methods: {
+    toggle: async function() {
+      this.toggling = true
+
+      await this.$API.put('user/me', {
+        hide: this.hidden ? 'yes' : 'no'
+      })
+
+      this.toggling = false
+    },
+  }
 }
 </script>
