0c00f935f56d6011104e832573b90895726447ac
Michael Wilson <michael_wilson@sil.org>
2023-02-20 13:28:57 +0930

    rename a var

--- src/2sv/key/Insert.vue
+++ src/2sv/key/Insert.vue
@@ -14,7 +14,7 @@
       <p v-else>{{ $vuetify.lang.t('$vuetify.2sv.key.insert.nosupport.info') }}</p>
 
       <!-- TODO: Add translations for this label -->
-      <label v-if="showModal">
+      <label v-if="showLabelInput">
         {{ $vuetify.lang.t('$vuetify.2sv.key.insert.label') }}
         <v-text-field v-model="input" required outlined autofocus/>
       </label>
@@ -27,11 +27,11 @@
 
       <v-spacer></v-spacer>
       
-      <v-btn v-if="isSupported && showModal" @click="onContinue" color="primary" outlined>
+      <v-btn v-if="isSupported && showLabelInput" @click="onContinue" color="primary" outlined>
         {{ $vuetify.lang.t('$vuetify.global.button.continue') }}
       </v-btn>
 
-      <v-btn v-else-if="isSupported  && !showModal" @click="onOk" color="primary" outlined>
+      <v-btn v-else-if="isSupported  && !showLabelInput" @click="onOk" color="primary" outlined>
         {{ $vuetify.lang.t('$vuetify.2sv.key.insert.button.ok') }}
       </v-btn>
       <v-btn v-else to="/2sv/printable-backup-codes/intro" color="primary" outlined> 	
@@ -56,7 +56,7 @@ export default {
   },
   data: () => ({	
     isSupported: browserSupportsWebauthn(),
-    showModal: false,
+    showLabelInput: false,
     input: '',
     newKeyName,
     snackbarIsOpen: false,
@@ -64,7 +64,7 @@ export default {
   }),
   methods: {
     onOk: function() {
-      this.showModal = true
+      this.showLabelInput = true
     },
     onContinue: function() {
       if(!this.input.trim()) {
