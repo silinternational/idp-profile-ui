f4a1621c66d61f5aac2f5fed27a039431fbcdc1f
Michael Wilson <michael_wilson@sil.org>
2023-05-11 10:00:25 +0930

    use gtag instead of analytics

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -1,8 +1,51 @@
-import Vue from 'vue'
-import VueAnalytics from 'vue-analytics'
 import configuredRouter from './router'
 
-Vue.use(VueAnalytics, {
-  id: `${process.env.VUE_APP_ANALYTICS_ID}`,
-  router: configuredRouter,
-})
+const GA_MEASUREMENT_ID = process.env.VUE_APP_ANALYTICS_ID || ''
+
+init()
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs
+// https://developers.google.com/analytics/devguides/collection/upgrade/analyticsjs
+function init() {
+  loadLib()
+
+  // TODO: get the dataLayer references to play nicer with ts
+  ;(window).dataLayer = (window).dataLayer || []
+  window.gtag = () => (window).dataLayer.push(arguments)
+
+  gtag('js', new Date())
+
+  //since we are sending manually we need to disable the default of sending each pageview
+  gtag('config', GA_MEASUREMENT_ID, {
+    send_page_view: false,
+  })
+
+  configuredRouter.beforeEach((to) => {
+    trackPageView(to.path)
+  })
+}
+
+function loadLib() {
+  const script = document.createElement('script')
+
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+  script.async = true
+
+  document.head.appendChild(script)
+}
+
+function trackPageView(page) {
+  if (page) {
+    // https://developers.google.com/analytics/devguides/collection/gtagjs/pages#default_behavior
+    gtag('event', 'page_view', {
+      page_path: location.pathname, //page.path or page.shortPath are also available
+    })
+  }
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+function trackEvent(eventName, eventParameters) {
+  gtag('event', eventName, eventParameters)
+}
+
+export const notFound = () => trackEvent('Error', 'Page not found')
