c87674c88a29c6c8ce19a1a1225007746810689e
Michael Wilson <michael_wilson@sil.org>
2023-05-15 12:51:26 +0930

    add a show password button

--- src/password/Confirm.vue
+++ src/password/Confirm.vue
@@ -7,9 +7,24 @@
 
       <v-form @submit.prevent="confirm" ref="form" class="pa-4">
         <p>{{ $vuetify.lang.t('$vuetify.password.confirm.explanation') }}</p>
-        
-        <BaseTextField type="password" :label="$vuetify.lang.t('$vuetify.password.confirm.pwInput')" v-model="password" 
-                       :rules="rules" :error-messages="errors" validate-on-blur @keyup.enter="blur" autofocus name="password" />
+
+        <div class="password">
+          <BaseTextField v-if="passwordIsHidden" type="password" :label="$vuetify.lang.t('$vuetify.password.confirm.pwInput')" v-model="password" 
+          :rules="rules" :error-messages="errors" validate-on-blur @keyup.enter="blur" autofocus name="password" />
+          
+          <BaseTextField v-else type="text" :label="$vuetify.lang.t('$vuetify.password.confirm.pwInput')" v-model="password" 
+          :rules="rules" :error-messages="errors" validate-on-blur @keyup.enter="blur" autofocus name="password" />
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn v-on="on" v-bind="attrs" class="eye" icon @click="passwordIsHidden = !passwordIsHidden" tabindex="-1">
+                <v-icon>{{ passwordIsHidden ? 'mdi-eye' : 'mdi-eye-off' }}</v-icon>
+              </v-btn>
+            </template>
+
+            <span>{{passwordIsHidden ? 'Show' : 'Hide'}} password</span>
+          </v-tooltip>
+        </div>
       </v-form>
     </BasePage>
 
@@ -36,6 +51,7 @@ export default {
   },
   data: vm => ({
     password: '',
+    passwordIsHidden: true,
     rules: [
       v => v == vm.$root.$data.password || vm.$vuetify.lang.t('$vuetify.password.confirm.noMatch')
     ],
@@ -64,3 +80,12 @@ export default {
   },
 }
 </script>
+
+<style>
+.password {
+  display: flex;
+}
+.eye {
+  margin-top: .75rem;
+}
+</style>
