28edb01b5945b76e61ba8d41274fdfaf87768307
Michael Wilson <michael_wilson@sil.org>
2024-03-11 15:49:46 +0930

    install vite andn try to get running

---
+++ vite.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    // This is necessary in Vue 2 codebases. It is automatic in Vue 3
+    __VUE_PROD_DEVTOOLS__: 'false',
+  },
+})
