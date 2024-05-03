bd7f4dec17641fa2a4dc753f161f35bda71d8350
Michael Wilson <michael_wilson@sil.org>
2024-05-03 09:57:00 +0930

    add environment and dsn env vars

--- src/main.js
+++ src/main.js
@@ -6,18 +6,22 @@ import { configuredRouter, configuredVuetify } from './plugins'
 import * as Sentry from '@sentry/vue'
 import Vue from 'vue'
 
+const environment = process.env.VUE_ENVIRONMENT
+const dsn = process.env.VUE_APP_SENTRY_DSN
+
+console.debug('Environment:', environment, 'DSN:', dsn)
 Sentry.init({
   Vue,
   dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
   integrations: [Sentry.browserTracingIntegration({ configuredVuetify }), Sentry.replayIntegration()],
-
+  environment,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 
   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
 
   // Capture Replay for 10% of all sessions,
   // plus for 100% of sessions with an error
