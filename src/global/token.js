4754dabc94f9b49c6747889cb262a5bfb185182f
Jason Jackson <jasonc.jackson@gmail.com>
2024-05-14 10:50:35 -0400

    use cookies instead of session for storing token

--- src/global/token.js
+++ src/global/token.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 init()
 
 export default {
@@ -5,7 +7,7 @@ export default {
   authzHeader: () => `${get('token_type')} ${apiToken()}`,
   reset,
   accessToken: () => get('access_token'),
-  setAccessToken: (t) => sessionStorage.setItem('access_token', t),
+  setAccessToken: (t) => Cookies.set('access_token', t, { expires: 7, path: '/' }),
   apiToken,
 }
 
@@ -25,19 +27,19 @@ function get(key) {
   // TODO: currently no IE support for URLSearchParams (https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams#Browser_compatibility)
   // check this out for further info:  https://developers.google.com/web/updates/2016/01/urlsearchparams
   // polyfill if needed (https://github.com/jerrybendy/url-search-params-polyfill) or the WebReflection one...
-  const defragged = new URL(location.href.replace('#',''))
+  const defragged = new URL(location.href.replace('#', ''))
   const params = new URLSearchParams(defragged.search)
 
-  return sessionStorage.getItem(key) || params.get(key)
+  return Cookies.get(key) || params.get(key)
 }
 
 function set(key, defaultValue = '') {
-  sessionStorage.setItem(key, get(key) || defaultValue)
+  Cookies.set(key, get(key) || defaultValue, { expires: 7, path: '/' })
 }
 
 function reset() {
-  sessionStorage.removeItem('token_type')
-  sessionStorage.removeItem('access_token')
+  Cookies.remove('token_type')
+  Cookies.remove('access_token')
 }
 
 function apiToken() {
