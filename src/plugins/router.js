bf492aa9554515024126c5320390b9b888da0e79
Michael Wilson <michael_wilson@sil.org>
2024-12-20 13:21:11 +0930

    use createWebHashHistory for compatibility with our urls

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import profileRoutes from '@/profile/routes'
 import passwordRoutes from '@/password/routes'
 import twoStepRoutes from '@/2sv/routes'
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
